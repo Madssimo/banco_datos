05531a1b5a1df640c4185dfd94b4b29c06fab1a6
Madssimo <giancarlo.araujo@up.ac.pa>
2020-04-16 13:15:57 -0500

    adding footer

--- banco-datos-app/src/components/App.js
+++ banco-datos-app/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from './Layout/Header';
+import Footer from './Layout/Footer';
 
 export default class extends Component {
   render(){
     return <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   }
 }
